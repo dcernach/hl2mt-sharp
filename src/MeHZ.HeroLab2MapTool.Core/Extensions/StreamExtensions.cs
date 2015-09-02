665d4b4a23ea6ed5f5e4f1b7f33d72e8e9a138c1
Dalton Andrade <4713577+dcernach@users.noreply.github.com>
2015-09-01 23:30:31 -0300

    - Initial Commit For now I can open and parse portifolio (.por) index and extract all relevant information required to parse all characters in portifolio.

---
+++ src/MeHZ.HeroLab2MapTool.Core/Extensions/StreamExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeHZ.HeroLab2MapTool.Core.Extensions {
+    public static class StreamExtensions {
+
+        public static string ComputeMD5(this Stream stream) {
+            if(!stream.CanSeek) {
+                throw new InvalidOperationException("Stream is not seekable.");
+            }
+            using (var md5 = MD5.Create()) {
+                var hash = md5.ComputeHash(stream);
+                var md5str = BitConverter.ToString(hash);
+                stream.Position = 0;
+                return md5str.Replace("-", "");
+            }
+        }
+    }
+}
